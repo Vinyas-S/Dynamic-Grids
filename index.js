d42b1c0b34451996f4c73c09d5def2b64d3f1b65
Vinyas <svinyas2000@gmail.com>
2023-06-29 10:41:16 +0530

    fixed code

--- index.js
+++ index.js
@@ -9,90 +9,89 @@ $(document).ready(function () {
       console.log(error);
     },
   });
+});
 
-  function renderGrids(data) {
-    var gridsContainer = $("#grids-container");
-    gridsContainer.empty();
+function renderGrids(data) {
+  var gridsContainer = $("#grids-container");
+  gridsContainer.empty();
 
-    $.each(data, function (index, machine) {
-      var grid = $("<div>", {
-        class: "card col-lg-4 col-md-6 col-sm-12",
-      });
-
-      var stateColor = getMachineStateColor(machine);
-      var stateIndicator = $("<div>", {
-        class: "state-indicator " + stateColor,
-        text: machine.name,
-      });
-
-      var machineState = getMachineState(machine);
-      var stateText = $('<span>', {
-          class: 'state-text',
-          text: machineState
-      });
-  
-      stateIndicator.append(stateText);
-
-      var gridBody = $("<div>", {
-        class: "card-body",
-      });
-
-      var metricsList = $("<ul>", {
-        class: "list-group list-group-flush",
-      });
-
-      $.each(machine.metrics, function (index, metric) {
-        var metricItem = $("<li>", {
-          class: "list-group-item",
-          text: metric.name + ": " + metric.value,
-        });
-
-        metricsList.append(metricItem);
-      });
-
-      gridBody.append(metricsList);
-      grid.append(stateIndicator, gridBody);
-
-      gridsContainer.append(grid);
+  $.each(data, function (index, machine) {
+    var grid = $("<div>", {
+      class: "card col-lg-4 col-md-6 col-sm-12",
     });
-  }
 
-  function getMachineStateColor(machine) {
-    var stateColor = "";
+    var stateColor = getMachineStateColor(machine);
+    var stateIndicator = $("<div>", {
+      class: "state-indicator " + stateColor,
+      text: machine.name,
+    });
+
+    var machineState = getMachineState(machine);
+    var stateText = $('<span>', {
+        class: 'state-text',
+        text: machineState
+    });
+
+    stateIndicator.append(stateText);
+
+    var gridBody = $("<div>", {
+      class: "card-body",
+    });
+
+    var metricsList = $("<ul>", {
+      class: "list-group list-group-flush",
+    });
 
     $.each(machine.metrics, function (index, metric) {
-      if (metric.name === "Running Since") {
-        stateColor = "bg-success";
-        return false;
-      } else if (metric.name === "Paused Since") {
-        stateColor = "bg-secondary";
-        return false;
-      } else if (metric.name === "Stopped Since") {
-        stateColor = "bg-danger";
-        return false;
-      }
+      var metricItem = $("<li>", {
+        class: "list-group-item",
+        text: metric.name + ": " + metric.value,
+      });
+
+      metricsList.append(metricItem);
     });
 
-    return stateColor;
-  }
+    gridBody.append(metricsList);
+    grid.append(stateIndicator, gridBody);
 
-  function getMachineState(machine) {
-    var machineState = '';
+    gridsContainer.append(grid);
+  });
+}
 
-    $.each(machine.metrics, function(index, metric) {
-      if (metric.name === 'Running Since') {
-        machineState = 'Running';
-        return false; 
-      } else if (metric.name === 'Paused Since') {
-        machineState = 'Paused';
-        return false;
-      } else if (metric.name === 'Stopped Since') {
-        machineState = 'Stopped';
-        return false;
-      }
-    });
+function getMachineStateColor(machine) {
+  var stateColor = "";
 
-    return machineState;
-  }
+  $.each(machine.metrics, function (index, metric) {
+    if (metric.name === "Running Since") {
+      stateColor = "bg-success";
+      return false;
+    } else if (metric.name === "Paused Since") {
+      stateColor = "bg-secondary";
+      return false;
+    } else if (metric.name === "Stopped Since") {
+      stateColor = "bg-danger";
+      return false;
+    }
+  });
 
-});
+  return stateColor;
+}
+
+function getMachineState(machine) {
+  var machineState = '';
+
+  $.each(machine.metrics, function(index, metric) {
+    if (metric.name === 'Running Since') {
+      machineState = 'Running';
+      return false; 
+    } else if (metric.name === 'Paused Since') {
+      machineState = 'Paused';
+      return false;
+    } else if (metric.name === 'Stopped Since') {
+      machineState = 'Stopped';
+      return false;
+    }
+  });
+
+  return machineState;
+}
