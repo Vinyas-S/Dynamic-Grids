11e19734cbbda135ff83e2569afad9b6c3083a61
Vinyas <svinyas2000@gmail.com>
2023-06-29 09:24:28 +0530

    modified css and added list items

--- index.js
+++ index.js
@@ -20,7 +20,26 @@ function renderGrids(data) {
       class: "grid col-lg-3 col-md-6 col-sm-12",
     });
 
-    
+    var gridBody = $('<div>', {
+      class: 'grid-body'
+    });
+
+    var metricsList = $('<ul>', {
+      class: 'list-group list-group-flush'
+    });
+
+    $.each(machine.metrics, function(index, metric) {
+      var metricItem = $('<li>', {
+        class: 'list-group-item',
+        text: metric.name + ': ' + metric.value
+      });
+
+      metricsList.append(metricItem);
+    });
+
+    gridBody.append(metricsList);
+    grid.append(stateIndicator, gridBody);
+
     gridsContainer.append(grid);
   });
 }
